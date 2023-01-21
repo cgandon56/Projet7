814daa1dbc963e2f98162c1b5ecfa5978e6372be
cgandon56 <108945404+cgandon56@users.noreply.github.com>
2023-01-21 16:57:33 +0100

    modification caroussel

--- src/components/Caroussel/caroussel.jsx
+++ src/components/Caroussel/caroussel.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+//import React from "react";
 import Carousel from 'react-bootstrap/Carousel'; // Récupération carousel de bootstrap
+//import data from '../../datas/data.json'
 
+import React, { useState } from 'react';
 
+//import back  from '../../images/back.svg';
+//import forward from '../../images/forward.svg';
 
+function Caroussel ({figures}) {
+  const [current] = useState(0); //je définie l'index du premier slide à 0
+	const length = figures.length; // props technique pour récupérer les images
+  return (
+    <Carousel interval={null} >  { // empêcher le déroulement automatique des slides
+      figures.map((figure, index) => { // pour renvoyer le tableau d'images
+        return (
+         
+          <Carousel.Item key={index} >
+              <img 
+              className="d-block w-100"
+              src= {figure}
+              alt="First slide"
+              />
+              <p className="caroussel_Number">
+							{index+1}/{length}
+						</p>
+          </Carousel.Item>
+          )
+        })
+      }  
+    </Carousel>
+  );
+}
+
+export default Caroussel
+
+/*
 function Caroussel ({figures}) { // props technique pour récupérer les images
   return (
     <Carousel interval={null} >  { // empêcher le déroulement automatique des slides
@@ -24,4 +56,8 @@ function Caroussel ({figures}) { // props technique pour récupérer les images
 
 export default Caroussel;
 
+ <div className='caroussel-count'>
+                <span>{picNumber}/{dataLength}</span>
+            </div>
+*/
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,12 +1,14 @@
 
 import Nav from '../Nav/nav';
 import logo from '../../images/LOGO.png';
-
+import { Link } from 'react-router-dom' ;
  
 function Header() {
 	return (
 		<header className="headerWrap">
+			<Link to="/">
 			<img className="logoHeader" src={logo} alt="logo de l'agence kasa" />
+			</Link>
 			<Nav className="nav_Header" />
 		</header>
 	);
