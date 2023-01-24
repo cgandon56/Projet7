d4edbfa131d53ff8f6e780ec6eb5888c69bab0f7
cgandon56 <108945404+cgandon56@users.noreply.github.com>
2023-01-24 16:40:41 +0100

    amelioration code

--- src/components/Caroussel/caroussel.jsx
+++ src/components/Caroussel/caroussel.jsx
@@ -4,17 +4,26 @@ import Carousel from 'react-bootstrap/Carousel'; // Récupération carousel de r
 
 function Caroussel ({figures}) { // props pour récupérer les images
 	const length = figures.length; 
+  
+
   return (
     <Carousel interval={null} >  { // empêcher le déroulement automatique des slides
       figures.map((figure, index) => { // pour renvoyer le tableau d'images
-        return ( 
+        if ( length === 1) {
+        return (
+          <Carousel.Item key={index}> 
+          <img className="d-(block w-100" src={figure} alt="images location" /> 
+          </Carousel.Item>)
+          } else {
+              return ( 
           <Carousel.Item key={index} > 
               <img className="d-block w-100" src= {figure} alt="images location" /> 
               <p className="caroussel_Number"> 
 							{index+1}/{length} 
-						  </p>
+						  </p> 
           </Carousel.Item> 
-          )
+          
+          )}
         }) 
       }  
     </Carousel> 
