6cc21cd074a29e6f8271d43d6eb38724cd420f22
cgandon56 <108945404+cgandon56@users.noreply.github.com>
2023-01-26 23:55:40 +0100

    modification carousel

--- src/components/Caroussel/caroussel.jsx
+++ src/components/Caroussel/caroussel.jsx
@@ -1,36 +1,98 @@
-import Carousel from 'react-bootstrap/Carousel'; // Récupération carousel de react-bootstrap
+import { useState } from 'react';
+import back from "../../images/back.svg" ;
+import forward from "../../images/forward.svg" ;
+
+function Carousel({ figures }) {
+	const [count, setCount] = useState(0); //je définie l'index du premier slide à 0
+	const length = figures.length; // longueur du tableau de slides
+
+	function handleNextClick ()  {
+		setCount(count === length - 1 ? 0 : count + 1); // on repart au premier slide quand on arrive au dernier
+	};
+	function handlePrevClick () {
+		setCount(count === 0 ? length - 1 : count - 1); // on repart au dernier slide quand on est au premier
+	};
+
+	return (
+		<div className ="carousel_Container">
+			{length > 1 && (
+				<img
+					src={forward} //Affichage des flèches seulement si length > 1
+					alt="forward"
+					onClick={handleNextClick}
+					className="arrow_Forward"
+				/>
+        
+			)}
+			{length > 1 && (
+				<img
+					src={back}
+					alt="previous"
+					onClick={handlePrevClick}
+					className="arrow_Back"
+				/>
+			)}
+      {length > 1 && (
+					<span className="carousel_Number">
+            {count + 1}/{length}
+          </span>
+			)}
+
+			{figures.map((figure, index) => (
+				<div key={index} className={count === index ? "carousel_Actif" : "carousel"}>
+					{index === count && <img src={figure} alt="location" className = "carousel_Img" />}
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default Carousel ;
 
 
 
-function Caroussel ({figures}) { // props pour récupérer les images
-	const length = figures.length; 
+
+
+
+/*
+
+function Caroussel ({figures}) {
+  const [index, setIndex] = useState(0);
+	
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handlePreviousClick() {
+    setIndex(index - 1);
+  }
   
-
+  const length = figures[index];
   return (
-    <Carousel interval={null} >  { // empêcher le déroulement automatique des slides
-      figures.map((figure, index) => { // pour renvoyer le tableau d'images
-        if ( length === 1) {
-        return (
-          <Carousel.Item key={index}> 
-          <img className="d-(block w-100" src={figure} alt="images location" /> 
-          </Carousel.Item>)
-          } else {
+    <div className="Caroussel" >  { 
+      figures.map((figure, index) => { 
+        
               return ( 
-          <Carousel.Item key={index} > 
-              <img className="d-block w-100" src= {figure} alt="images location" /> 
-              <p className="caroussel_Number"> 
-							{index+1}/{length} 
-						  </p> 
-          </Carousel.Item> 
-          
-          )}
-        }) 
-      }  
-    </Carousel> 
+                <>
+                <button onClick={handleNextClick}>
+        Next
+      </button>
+                    <img className="Carousel_Img_Logement" src= {figure} alt="images location" /> 
+                    <p className="caroussel_Number"> 
+                      {index+1}/{length} 
+                    </p> 
+                
+              
+              </> 
+      )})
+        }
+       
+    </div> 
   );
 }
 
-export default Caroussel
+export default Caroussel */
 
 
 
