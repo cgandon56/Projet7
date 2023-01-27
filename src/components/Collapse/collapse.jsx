4b6d8c0ea591e205dca7b8e9e72aeaac1a4f9860
cgandon56 <108945404+cgandon56@users.noreply.github.com>
2023-01-27 08:20:57 +0100

    modification collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,6 +1,35 @@
-import Accordion from 'react-bootstrap/Accordion'; // récupération accordéo de react-bootstrap
+import { useState } from 'react';
+import Vector from "../../images/VectorCollapse.svg" ;
 
 
+
+function Collapse(props) {
+  const [showMore, setShowMore] = useState(false);
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+  return (
+    <>
+    <div onClick={handleMoreClick} className ="button_Collapse" >
+      <p className="collapse_Text" >{props.title}</p>
+       
+    < img className = "collapse_Icon" src={Vector} alt ="Vector" ></img>
+      
+    </div>
+      <div className = "collapse_Content">
+      {showMore&& <p className="collapse_Open" >{props.description}</p>}
+      </div>
+      </>
+  )
+}
+
+export default Collapse ;
+
+
+
+
+
+/*
 function Collapse(props) { // props pour récupérer les titres et descriptions
   return (
     <Accordion>
@@ -14,4 +43,4 @@ function Collapse(props) { // props pour récupérer les titres et descriptions
   );
 }
   
-  export default Collapse
+  export default Collapse*/
--- src/pages/Propos.jsx
+++ src/pages/Propos.jsx
@@ -8,7 +8,7 @@ function Propos() {
     return (
         <>
              <Banner img = {imgbanner_about}/> 
-                   <div className="container">
+                   <div className="container_Collapse">
                         <Collapse title = "Fiabilité" description = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
                         <Collapse title = "Respect" description = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme. " />
                         <Collapse title = "Service" description = "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
